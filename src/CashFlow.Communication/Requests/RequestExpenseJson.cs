2ae0f35275e66c960308f76113d70f0f5a75ffb3
abraao garcia <abraaogarcia54@gmail.com>
2025-06-04 13:42:39 -0300

    segundo commit test

--- src/CashFlow.Communication/Requests/RequestExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestExpenseJson.cs
@@ -6,5 +6,6 @@
         public string Description { get; set; } = string.Empty;
         public DateTime Date { get; set; }
         public decimal Amount { get; set; }
+        public PaymentType paymentType { get; set; }
     }
 }
