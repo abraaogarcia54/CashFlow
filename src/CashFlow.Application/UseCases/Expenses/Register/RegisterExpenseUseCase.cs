4fee30468920e7edf3401234027bb2075f3f0aa8
abraao garcia <abraaogarcia54@gmail.com>
2025-09-04 14:24:04 -0300

    mexendo na application

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
@@ -7,9 +8,39 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
-            //to do validation
+            Validate(request);
 
             return new ResponseRegisteredExpenseJson();
         }
+
+
+        public void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            if (titleIsEmpty)
+            {
+                throw new ArgumentException("The title is required.");
+            }
+
+            if (request.Amount <= 0)
+            {
+                throw new ArgumentException("The amount must be greater than zero.");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if(result > 0)
+            {
+                throw new Exception("Expenses cannot be for the future.");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.paymentType);
+            if (paymentTypeIsValid == false)
+            {
+                throw new ArgumentException("The payment type is invalid.");
+            }
+
+        }
     }
-}
+
+}   
+    
